95a80521603d6b9f14ff4b7e8587504113b04ca3
Naresh <ghostwriternr@gmail.com>
2017-08-08 04:09:54 +0530

    Removed some dead code

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 // import logo from './logo.svg';
 import Name from '../components/Name';
 import Experience from './Experience';
-import Navigation from '../components/Navigation';
 import '../styles/App.css';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
@@ -10,11 +9,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-          {/* <Name /> */}
         <Route exact path="/" component={Name}/> 
         <Route path="/experience" component={Experience}/>
-        <Navigation /> 
-        {/* <Experience /> */}
       </div>
     );
   }
