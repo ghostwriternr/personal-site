05162270988d232cb2d6cb26887ac26b0ad4c1d8
Naresh <ghostwriternr@gmail.com>
2017-08-21 02:27:26 +0530

    About page done and responsive

--- src/containers/About.jsx
+++ src/containers/About.jsx
@@ -29,15 +29,32 @@ class About extends Component {
                         <text id="about-hi">Hi, I'm Naresh</text>
                     </div>
                 </div>
-                <div className="row visible-md visible-lg" id="aboutdescdesk">
+                <div className="row mobile-invisible text-center" id="aboutdescdesk">
                     <div className="col-xs-12 col-md-offset-7 col-md-4 text-center" id="intro">
                         <text>I am a developer, writer and designer, aiming to bring the best of these 3 worlds together through Computer Science. I enjoy programming and listening to music.</text>
                     </div>
                 </div>
-                <div className="row visible-xs visible-sm" id="aboutdescmob">
+                <div className="row mobile-invisible text-center" id="aboutimg">
+                    <div className="col-xs-12 col-md-offset-7 col-md-4 text-center" id="profileimg">
+                        <img className="about-image img-thumbnail" src={require('../images/about-bw.jpeg')} alt="Me!" />
+                    </div>
+                </div>
+                <div className="stick-bottom">
+                <div className="row desktop-invisible" id="aboutdescmob">
                     <div className="col-xs-12 col-md-offset-6 col-md-6 text-center" id="intro">
                         <text>I am a developer, writer and designer, aiming to bring the best of these 3 worlds together through Computer Science. I enjoy programming and listening to music.</text>
                     </div>
+                </div>
+                <div className="row" id="iconlinks">
+                    <div className="col-xs-12 col-md-offset-7 col-md-4 text-center">
+                        <a href='https://github.com/ghostwriternr/' target="_blank" rel="noopener noreferrer"><i id="githubicon" className="icon ion-social-github link-icons"></i></a>
+                        <a href='https://www.linkedin.com/in/naresh-r-464a8b8b' target="_blank" rel="noopener noreferrer"><i id="linkedinicon" className="icon ion-social-linkedin link-icons"></i></a>
+                        <a href='https://www.facebook.com/naresh.ramesh' target="_blank" rel="noopener noreferrer"><i id="facebookicon" className="icon ion-social-facebook link-icons"></i></a>
+                        <a href='http://blog.ghostwriternr.me/' target="_blank" rel="noopener noreferrer"><i id="blogicon" className="icon ion-edit link-icons"></i></a>
+                        <a href='https://www.instagram.com/naresh_r/' target="_blank" rel="noopener noreferrer"><i id="instaicon" className="icon ion-social-instagram-outline link-icons"></i></a>
+                        <a href='mailto:nareshmdu@gmail.com' target="_blank" rel="noopener noreferrer"><i id="mailicon" className="icon ion-ios-email link-icons"></i></a>
+                    </div>
+                </div>
                 </div>
             </div>
         );
