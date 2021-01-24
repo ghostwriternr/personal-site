083d3974070c9538e19f125a4728f560db3f76d0
Naresh R <ghostwriternr@gmail.com>
2021-01-24 20:40:35 +0530

    Add social icons

--- components/body.tsx
+++ components/body.tsx
@@ -1,19 +1,38 @@
+import { Facebook, Github, Gmail, Goodreads, Instagram, Linkedin } from "@icons-pack/react-simple-icons";
+
 export function Body() {
     return (
         <>
-            <div className="grid grid-cols-12 mt-8 items-center">
+            <div className="mt-8 grid grid-cols-12 items-center">
                 <div className="col-start-3 col-span-6">
-                    <h1 className="flex text-4xl mb-8 font-semibold" style={{ color: "#47184c" }}>
+                    <h1 className="flex text-4xl font-semibold" style={{ color: "#47184c" }}>
                         <div className="mr-2">Hello</div>&#8226;<div className="ml-2 mr-2 hindi">नमस्ते</div>&#8226;
                         <div className="ml-2 mr-2 tamil">வணக்கம்</div>&#8226;
                         <div className="ml-2 telugu">స్వాగతం</div>
                     </h1>
-                    <div className="grid grid-cols-5">
-                        <h2 className="col-span-4 text-2xl">
-                            I'm a software engineer, currently working at Setu. I enjoy writing, curating music and
-                            playing the Piano. Open Source maintainer &amp; supporter. Dreaming of the sky and a
-                            prosperous India.
-                        </h2>
+                    <h2 className="mt-8 col-span-4 text-2xl">
+                        I'm a software engineer, currently working at Setu. I enjoy writing, curating music and playing
+                        the Piano. Open Source maintainer &amp; supporter. Dreaming of the sky and a prosperous India.
+                    </h2>
+                    <div className="mt-4 flex space-x-4">
+                        <a href="https://github.com/ghostwriternr" target="_blank" rel="noopener noreferrer">
+                            <Github />
+                        </a>
+                        <a href="https://www.linkedin.com/in/naresh-ramesh" target="_blank" rel="noopener noreferrer">
+                            <Linkedin />
+                        </a>
+                        <a href="mailto:ghostwriternr@gmail.com">
+                            <Gmail />
+                        </a>
+                        <a href="https://www.facebook.com/naresh.ramesh" target="_blank" rel="noopener noreferrer">
+                            <Facebook />
+                        </a>
+                        <a href="https://www.instagram.com/noresh.romesh" target="_blank" rel="noopener noreferrer">
+                            <Instagram />
+                        </a>
+                        <a href="https://www.goodreads.com/ghostwriternr" target="_blank" rel="noopener noreferrer">
+                            <Goodreads />
+                        </a>
                     </div>
                 </div>
                 <div
